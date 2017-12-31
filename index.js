61e3a5da67b0f3bbb53d363490544df3af686b7d
Kwinten Van den Berghe <kwintenvdberghe@gmail.com>
2017-12-31 16:38:02 +0100

    Added percentage info

--- index.js
+++ index.js
@@ -17,22 +17,25 @@ client.on("message", async message => {
 	if (message.content === "!votes") {
 		let reply = await message.channel.send("Getting vote standings...");
 		const res = await request(reqOptions);
+		const totalVotes = res.voteList[0].voteCount;
+
 		let voteOptions = res.voteList[0].optionList;
 		voteOptions = voteOptions.sort((a, b) => {
 			return b.voteNumber - a.voteNumber;
 		}).slice(0, 5);
 		reply.edit({ embed: {
 			description: "Binance community vote - top 5\nGo [here](https://www.binance.com/vote.html) to vote.",
-			fields: toFields(voteOptions)
+			fields: toFields(voteOptions, totalVotes)
 		}});
 	}
 });
 
-function toFields(voteOptions) {
+function toFields(voteOptions, totalVotes) {
 	return voteOptions.map(v => {
+		const percentage = (v.voteNumber / totalVotes) * 100;
 		return {
 			name: v.optionName,
-			value: v.voteNumber,
+			value: `${v.voteNumber} (${percentage.toFixed(2)}%)`,
 			inline: false
 		};
 	});
